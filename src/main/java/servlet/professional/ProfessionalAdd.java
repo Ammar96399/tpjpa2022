7ff8a7888aa95124beea3b9e42b2d628ac29d516
nep-nep327 <91326644+nep-nep327@users.noreply.github.com>
2022-09-12 14:18:05 +0200

    [HWAN] TP2 Question 5 - fixed a problem that professionals won't be inserted as the form is filled

--- src/main/java/servlet/professional/ProfessionalAdd.java
+++ src/main/java/servlet/professional/ProfessionalAdd.java
@@ -19,21 +19,12 @@ public class ProfessionalAdd extends HttpServlet {
 
         PrintWriter out = response.getWriter();
 
-        System.out.println(2);
-
         var lastname = request.getParameter("lastname");
         var firstname = request.getParameter("firstname");
 
-        System.out.println(lastname);
-        System.out.println(firstname);
-
         var dao = new ProfessionalDAO();
 
-        dao.getAll().forEach(e -> System.out.println(e.getFirstName()));
-
         dao.createProfessional(lastname, firstname);
-
-        dao.getAll().forEach(e -> System.out.println(e.getFirstName()));
 
         out.println("Ajout d'un professionnel avec succès.");
     }
